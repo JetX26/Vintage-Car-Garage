8ea04d3d83a7ca535990adbfb510f64a2f5f98f9
JetX26 <kocevstanoj@gmail.com>
2023-11-18 02:00:53 -0600

    bug fixes

--- app/components/Bookonline.tsx
+++ app/components/Bookonline.tsx
@@ -10,15 +10,17 @@ const Bookonline = (): React.ReactNode => {
         <>
             <div>
                 <Navbar></Navbar>
-                <div className=' flex flex-col items-center p-32 gap-12'>
-                    <div>
-                        <h1 className=' text-4xl'>BOOK ONLINE</h1>
+                <div className=' flex flex-col items-center p-32 gap-12 xs:p-0 sm:p-6'>
+                    <div className='h-[50%]'>
+                        <div className=' flex'>
+                            <h1 className=' text-4xl'>BOOK ONLINE</h1>
+                        </div>
                     </div>
-                    <div className=' flex justify-center'>
+                    <div className=' flex justify-center xs:flex-col xs:mb-2 sm:flex-col sm:mb-2 md:flex-col'>
                         <div className=' border-black border-[2px]'>
                             <Image className=' h-full' src={Bookimage} alt="Car Steering Wheel"></Image>
                         </div>
-                        <div className='p-20 relative text-center flex flex-col justify-center items-center gap-4 border-black border-[1px] border-l-0'>
+                        <div className='p-20 relative text-center flex flex-col justify-center items-center gap-4 border-black border-[1px] border-l-0 xs:border-l-[1px] xs:gap-1 xs:p-4 sm:border-l-[1px] sm:gap-1 sm:p-2 md:gap-0 md:p-6 md:border-l-[1px]'>
                             <h2 className=' text-xl'>Style My Ride - Consultation</h2>
                             <p>2 hr</p>
                             <p>$100</p>
